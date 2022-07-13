acc7022d711cdb9eb1d86652df9c6bb822576192
BaraSec <barasec@protonmail.com>
2022-07-13 19:45:35 +0400

    understanding redux

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React from 'react'
 import useStyles from './styles'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
 
 const Form = () => {
     const classes = useStyles()
 
+    const handleSubmit = () => {
+        
+    }
+
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant='h6'>Creating a Memory</Typography>
+                <TextField 
+                    name='creator' 
+                    variant='outlined' 
+                    label='Creator' 
+                    fullWidth 
+                    // value={} 
+                    // onChange={}
+                />
+            </form>
+        </Paper>
     )
 }
 
